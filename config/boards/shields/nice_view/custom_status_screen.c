107d541188880ae373e0d0ec46250c5c81c4568a
ciutadellla <pedro.aspiazu@gmail.com>
2025-02-19 17:45:12 +0100

    bongo

--- config/boards/shields/nice_view/custom_status_screen.c
+++ config/boards/shields/nice_view/custom_status_screen.c
@@ -5,8 +5,8 @@
  *
  */
 
-// #include "widgets/status.h"
-// #include <widgets/bongo_cat.h>
+#include "widgets/status.h"
+
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
@@ -14,24 +14,15 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 static struct zmk_widget_status status_widget;
 #endif
 
-static struct zmk_widget_bongo_cat bongo_cat_widget;
-
-
 lv_obj_t *zmk_display_status_screen() {
 
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
 
-
-
-    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
-
-
-    #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
-        zmk_widget_status_init(&status_widget, screen);
-        lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
-    #endif
+#if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
+    zmk_widget_status_init(&status_widget, screen);
+    lv_obj_align(zmk_widget_status_obj(&status_widget), LV_ALIGN_TOP_LEFT, 0, 0);
+#endif
 
     return screen;
 }
