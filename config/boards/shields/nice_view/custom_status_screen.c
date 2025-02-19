2a8a66db7921a04675c94209a5b7b8c0fb778c5a
ciutadellla <pedro.aspiazu@gmail.com>
2025-02-19 16:56:28 +0100

    bongo

--- config/boards/shields/nice_view/custom_status_screen.c
+++ config/boards/shields/nice_view/custom_status_screen.c
@@ -17,8 +17,7 @@ static struct zmk_widget_status status_widget;
 
 #include <zmk/display/widgets/bongo_cat.h>
 static struct zmk_widget_bongo_cat bongo_cat_widget;
-zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
-lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
+
 
 
 
@@ -28,6 +27,9 @@ lv_obj_t *zmk_display_status_screen() {
 
     lv_obj_t *screen;
     screen = lv_obj_create(NULL);
+
+    zmk_widget_bongo_cat_init(&bongo_cat_widget, screen);
+    lv_obj_align(zmk_widget_bongo_cat_obj(&bongo_cat_widget), NULL, LV_ALIGN_CENTER, 0, 0);
 
 #if IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_STATUS)
     zmk_widget_status_init(&status_widget, screen);
